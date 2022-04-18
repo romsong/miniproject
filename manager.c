33184bcc9da75715da931316402e8171c84641d0
romsong <22100107@handong.ac.kr>
2022-04-18 22:15:02 +0900

    menu 추가

--- manager.c
+++ manager.c
@@ -9,6 +9,10 @@ int selectMenu() // 메뉴선택 창
     printf("2. 추가\n");
     printf("3. 수정\n");
     printf("4. 삭제\n");
+    printf("5. 저장\n");
+    printf("6. 가격 검색\n");
+    printf("7. 단위 검색\n");
+    printf("8. 이름 검색\n");
     printf("0. 종료\n");
 
     printf("=> 원하는 메뉴는? ");
